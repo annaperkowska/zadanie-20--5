28dbdfa0dae3fe9e8b9f0b5224af912491aba00d
Anna Perkowska <perkowska.a@gmail.com>
2018-05-15 21:45:20 +0200

    Create repo

---
+++ src/presentational/contact.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => (
+	<div>
+		<h1>Search Country app</h1>
+		<h2>Route 66</h2>
+		<h2>Memphis</h2>
+		<h2>tel. 123 456 789</h2>
+	</div>
+);
+
+export default Contact;
